c6554b6a12d052fea12cdfeab045bd4fc4806e5b
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-07-14 09:19:46 -0700

    bump build

--- example/bundle/i3dmExample.c6cc930a.js
+++ example/bundle/i3dmExample.c6cc930a.js
@@ -36516,7 +36516,7 @@ function skipTraversal(tile, renderer) {
 
       tile.__active = true;
       stats.active++;
-    } else if (!lruCache.isFull() && !tile.__contentEmpty) {
+    } else if (!lruCache.isFull()) {
       renderer.requestTileContents(tile);
     }
 
@@ -42907,7 +42907,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50124" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51194" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
