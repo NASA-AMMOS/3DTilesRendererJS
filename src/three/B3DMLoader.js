2823729b08b6756ff48460fd71bcf39f03dc20a7
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-07-20 16:11:34 -0700

    Update B3DMLoader.js

--- src/three/B3DMLoader.js
+++ src/three/B3DMLoader.js
@@ -7,6 +7,27 @@ export class B3DMLoader extends B3DMLoaderBase {
 
 		super();
 		this.manager = manager;
+		this.ktx2Loader = null;
+		this.dracoLoader = null;
+		this.ddsLoader = null;
+
+	}
+	
+	setKTX2Loader( loader ) {
+
+		this.ktx2Loader = loader;
+
+	}
+	
+	setDracoLoader( loader ) {
+
+		this.dracoLoader = loader;
+
+	}
+	
+	setDDSLoader( loader ) {
+
+		this.ddsLoader = loader;
 
 	}
 
@@ -17,7 +38,11 @@ export class B3DMLoader extends B3DMLoaderBase {
 		return new Promise( ( resolve, reject ) => {
 
 			const manager = this.manager;
-			new GLTFLoader( manager ).parse( gltfBuffer, null, model => {
+			const loader = new GLTFLoader( manager );
+			loader.setKTX2Loader( this.ktx2Loader );
+			loader.setDracoLoader( this.dracoLoader );
+			loader.setDDSLoader( this.ddsLoader );
+			loader.parse( gltfBuffer, null, model => {
 
 				model.batchTable = b3dm.batchTable;
 				model.featureTable = b3dm.featureTable;
