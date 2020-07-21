776cfd8a3739c8997f5c996cd5e2e50494c1543c
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-07-20 18:39:13 -0700

    Update I3DMLoader.d.ts

--- src/three/I3DMLoader.d.ts
+++ src/three/I3DMLoader.d.ts
@@ -1,8 +1,5 @@
 import { I3DMBaseResult } from '../base/I3DMLoaderBase';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
-import { DracoLoader } from 'three/examples/jsm/loaders/DracoLoader';
-import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
-import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader';
 
 export interface I3DMResult extends GLTF, I3DMBaseResult {
 
@@ -13,16 +10,8 @@ export interface I3DMResult extends GLTF, I3DMBaseResult {
 
 export class I3DMLoader {
 
-	dracoLoader : DracoLoader | null;
-	ddsLoader : DDSLoader | null;
-	ktx2Loader : KTX2Loader | null;
-	
 	constructor( manager : LoadingManager );
 	load( url : String ) : Promise< I3DMResult >;
 	parse( buffer : ArrayBuffer ) : I3DMResult;
-
-	setDracoLoader( loader : DracoLoader | null ) : void;
-	setDDSLoader( loader : DDSLoader | null ) : void;
-	setKTX2Loader( loader : KTX2Loader | null ) : void;
 	
 }
