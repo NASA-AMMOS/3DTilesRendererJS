dd9f9a519048227b9db549fb94dc2a6a41095696
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-07-20 18:41:04 -0700

    Update B3DMLoader.d.ts

--- src/three/B3DMLoader.d.ts
+++ src/three/B3DMLoader.d.ts
@@ -1,6 +1,9 @@
 import { B3DMBaseResult } from '../base/B3DMLoaderBase';
 import { LoadingManager } from 'three';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DracoLoader } from 'three/examples/jsm/loaders/DracoLoader';
+import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
+import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader';
 
 export interface B3DMResult extends GLTF, B3DMBaseResult {}
 
@@ -9,5 +12,9 @@ export class B3DMLoader {
 	constructor( manager : LoadingManager );
 	load( url : String ) : Promise< B3DMResult >;
 	parse( buffer : ArrayBuffer ) : B3DMResult;
+	
+	setDracoLoader( loader : DracoLoader | null ) : void;
+	setDDSLoader( loader : DDSLoader | null ) : void;
+	setKTX2Loader( loader : KTX2Loader | null ) : void;
 
 }
