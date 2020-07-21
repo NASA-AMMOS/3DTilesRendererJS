d98e3ecfd1983c77b74c17e0880b81f917f930de
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-07-20 18:41:01 -0700

    Update TilesRenderer.d.ts

--- src/three/TilesRenderer.d.ts
+++ src/three/TilesRenderer.d.ts
@@ -1,6 +1,9 @@
 import { Box3, Camera, Vector2, WebGLRenderer, Object3D } from 'three';
 import { TilesRendererBase } from '../base/TilesRendererBase';
 import { TilesGroup } from './TilesGroup';
+import { DracoLoader } from 'three/examples/jsm/loaders/DracoLoader';
+import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
+import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader';
 
 export class TilesRenderer extends TilesRendererBase {
 
@@ -21,5 +24,9 @@ export class TilesRenderer extends TilesRendererBase {
 	onLoadModel : ( ( scene : Object3D, tile : object ) => void ) | null;
 	onDisposeModel : ( ( scene : Object3D, tile : object ) => void ) | null;
 	forEachLoadedModel( callback : ( scene : Object3D, tile : object ) => void );
+
+	setDracoLoader( loader : DracoLoader | null ) : void;
+	setDDSLoader( loader : DDSLoader | null ) : void;
+	setKTX2Loader( loader : KTX2Loader | null ) : void;
 
 }
