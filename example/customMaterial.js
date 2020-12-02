c44e3f7a3d4d338d8af827c1f3e5e19b0ed1c89e
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-12-02 09:24:29 -0800

    Fix shader

--- example/customMaterial.js
+++ example/customMaterial.js
@@ -96,7 +96,7 @@ const topoShader = {
 
 			// thickness scale
 			float upwardness = dot( worldNormal, vec3( 0.0, 1.0, 0.0 ) );
-			float yInv = saturate( 1.0 - abs( upwardness ) );
+			float yInv = clamp( 1.0 - abs( upwardness ), 0.0, 1.0 );
 			float thicknessScale = pow( yInv, 0.4 );
 			thicknessScale *= 0.25 + 0.5 * ( vViewPosition.z + 1.0 ) / 2.0;
 
