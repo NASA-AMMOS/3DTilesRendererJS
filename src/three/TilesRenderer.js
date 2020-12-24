e40bd51a83e65d5c49d9549bd3430035a0c2a322
Petter <petter.gasstrom@tridify.com>
2020-12-24 12:10:49 +0200

    Removed legacy three projection function

--- src/three/TilesRenderer.js
+++ src/three/TilesRenderer.js
@@ -292,25 +292,6 @@ export class TilesRenderer extends TilesRendererBase {
 
 	}
 
-	/** set projection matrix to frustum for legacy versions of three.js */
-	setFromProjectionMatrix( frustum, m ) {
-
-		var planes = frustum.planes;
-		var me = m.elements;
-		var me0 = me[ 0 ], me1 = me[ 1 ], me2 = me[ 2 ], me3 = me[ 3 ];
-		var me4 = me[ 4 ], me5 = me[ 5 ], me6 = me[ 6 ], me7 = me[ 7 ];
-		var me8 = me[ 8 ], me9 = me[ 9 ], me10 = me[ 10 ], me11 = me[ 11 ];
-		var me12 = me[ 12 ], me13 = me[ 13 ], me14 = me[ 14 ], me15 = me[ 15 ];
-
-		planes[ 0 ].setComponents( me3 - me0, me7 - me4, me11 - me8, me15 - me12 ).normalize();
-		planes[ 1 ].setComponents( me3 + me0, me7 + me4, me11 + me8, me15 + me12 ).normalize();
-		planes[ 2 ].setComponents( me3 + me1, me7 + me5, me11 + me9, me15 + me13 ).normalize();
-		planes[ 3 ].setComponents( me3 - me1, me7 - me5, me11 - me9, me15 - me13 ).normalize();
-		planes[ 4 ].setComponents( me3 - me2, me7 - me6, me11 - me10, me15 - me14 ).normalize();
-		planes[ 5 ].setComponents( me3 + me2, me7 + me6, me11 + me10, me15 + me14 ).normalize();
-
-	}
-
 	update() {
 
 		const group = this.group;
@@ -390,20 +371,12 @@ export class TilesRenderer extends TilesRendererBase {
 
 			info.invScale = invScale;
 
-			// get frustum in grop root frame
+			// get frustum in group root frame
 			tempMat.copy( group.matrixWorld );
 			tempMat.premultiply( camera.matrixWorldInverse );
 			tempMat.premultiply( camera.projectionMatrix );
 
-			if ( typeof frustum.setFromProjectionMatrix === 'function' ) {
-
-				frustum.setFromProjectionMatrix( tempMat );
-
-			} else {
-
-				this.setFromProjectionMatrix( frustum, tempMat );
-
-			}
+			frustum.setFromProjectionMatrix( tempMat );
 
 			// get transform position in group root frame
 			position.set( 0, 0, 0 );
