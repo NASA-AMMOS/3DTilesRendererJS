d970370a47f9b2081281c382aed9dd472e9307e3
PatrickNausha <patrick.nausha@gmail.com>
2021-08-21 11:07:59 -0700

    Make test cases more generic.

--- test/urlExtension.test.js
+++ test/urlExtension.test.js
@@ -3,12 +3,12 @@ import { getUrlExtension } from '../src/utilities/urlExtension.js';
 describe( 'getUrlExtension', () => {
 
 	it.each( [
-		'https://cloud.skylineglobe.com/sg/b3dm/NeveTzedek3.420160/tileset.json',
-		'https://cloud.skylineglobe.com/sg/b3dm/NeveTzedek3.420160/tileset.json?foo=bar',
-		'https://cloud.skylineglobe.com/sg/b3dm/NeveTzedek3.420160/tileset.json?a.b=c.d',
-		'https://cloud.skylineglobe.com/tileset.json',
-		'https://cloud.skylineglobe.com//tileset.json',
-		'file:///Users/paigemansfield/patricks-code/3DTilesRendererJS/example/b3dmExample.json',
+		'https://nasa.gov/foo/bar/baz.qux/tileset.json',
+		'https://nasa.gov/foo/bar/baz.qux/tileset.json?foo=bar',
+		'https://nasa.gov/foo/bar/baz.qux/tileset.json?a.b=c.d',
+		'https://nasa.gov/tileset.json',
+		'https://nasa.gov//tileset.json',
+		'file:///Users/JaneScientist/code/3DTilesRendererJS/example/b3dmExample.json',
 		'foo.json',
 		'foo.json?a=b',
 		'/foo.json',
@@ -19,12 +19,14 @@ describe( 'getUrlExtension', () => {
 	} );
 
 	it.each( [
-		'https://cloud.skylineglobe.com/tileset',
-		'https://cloud.skylineglobe.com/sg/b3dm/NeveTzedek3.420160/tileset?foo=bar',
-		'https://cloud.skylineglobe.com/sg/b3dm/NeveTzedek3.420160/tileset?a.b=c.d',
-		'https://cloud.skylineglobe.com/tileset',
-		'https://cloud.skylineglobe.com/tileset.',
-		'bacon',
+		'https://nasa.gov',
+		'https://nasa.gov/',
+		'https://nasa.gov/tileset',
+		'https://nasa.gov/sg/b3dm/NeveTzedek3.420160/tileset?foo=bar',
+		'https://nasa.gov/sg/b3dm/NeveTzedek3.420160/tileset?a.b=c.d',
+		'https://nasa.gov/tileset',
+		'https://nasa.gov/tileset.',
+		'Pleiades',
 	] )( 'returns null for strings that are not URLs with extensions %s', url => {
 
 		expect( getUrlExtension( url ) ).toBeNull();
