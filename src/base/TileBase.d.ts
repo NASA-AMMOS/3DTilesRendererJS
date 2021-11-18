27ddb9cbb12f82cd3cd9d21b1df627822d245456
Dave Buchhofer <buchhofer@matterport.com>
2021-11-18 15:47:04 -0500

    [types] - cleanup some tabs/spaces issues

--- src/base/TileBase.d.ts
+++ src/base/TileBase.d.ts
@@ -8,28 +8,28 @@ export interface TileBase {
 
 	boundingVolume: {
 
-        /**
-         * An array of 12 numbers that define an oriented bounding box. The first three elements define the x, y, and z
-         * values for the center of the box. The next three elements (with indices 3, 4, and 5) define the x axis
-         * direction and half-length. The next three elements (indices 6, 7, and 8) define the y axis direction and
-         * half-length. The last three elements (indices 9, 10, and 11) define the z axis direction and half-length.
-         */
-        box?: number[];
+		/**
+		 * An array of 12 numbers that define an oriented bounding box. The first three elements define the x, y, and z
+		 * values for the center of the box. The next three elements (with indices 3, 4, and 5) define the x axis
+		 * direction and half-length. The next three elements (indices 6, 7, and 8) define the y axis direction and
+		 * half-length. The last three elements (indices 9, 10, and 11) define the z axis direction and half-length.
+		 */
+		box?: number[];
 
-        /**
-         * An array of four numbers that define a bounding sphere. The first three elements define the x, y, and z
-         * values for the center of the sphere. The last element (with index 3) defines the radius in meters.
-         */
-        sphere?: number[];
+		/**
+		 * An array of four numbers that define a bounding sphere. The first three elements define the x, y, and z
+		 * values for the center of the sphere. The last element (with index 3) defines the radius in meters.
+		 */
+		sphere?: number[];
 
-    };
+	};
 
-    /**
-     * The error, in meters, introduced if this tileset is not rendered. At runtime, the geometric error is used to compute screen space error (SSE), i.e., the error measured in pixels.
-     */
-    geometricError: number;
+	/**
+	 * The error, in meters, introduced if this tileset is not rendered. At runtime, the geometric error is used to compute screen space error (SSE), i.e., the error measured in pixels.
+	 */
+	geometricError: number;
 
-    // optional properties
+	// optional properties
 
 	children?: TileBase[];
 
@@ -44,7 +44,7 @@ export interface TileBase {
 
 		extras?: Record<string, any>;
 
-    	// Non standard, noted here as it exists in the code in this package to support old pre-1.0 tilesets
+		// Non standard, noted here as it exists in the code in this package to support old pre-1.0 tilesets
 		url?: string;
 
 	};
--- src/base/Tileset.d.ts
+++ src/base/Tileset.d.ts
@@ -7,60 +7,60 @@ import { TileBase } from './TileBase';
  */
 export interface Tileset {
 
-    /**
-     * Metadata about the entire tileset.
-     */
-    asset: {
+	/**
+	 * Metadata about the entire tileset.
+	 */
+	asset: {
 
-        /**
-         * 3d-tiles version
-         */
-        version: string,
+		/**
+		 * 3d-tiles version
+		 */
+		version: string,
 
-        /**
-         * Application specific version
-         */
-        tilesetVersion?: string,
+		/**
+		 * Application specific version
+		 */
+		tilesetVersion?: string,
 
-        /**
-	     * Dictionary object with extension-specific objects.
-	     */
-        extensions? : Record<string, any>,
+		/**
+		 * Dictionary object with extension-specific objects.
+		 */
+		extensions? : Record<string, any>,
 
-    };
+	};
 
-    /**
-     * The error, in meters, introduced if this tileset is not rendered. At runtime, the geometric error is used to compute screen space error (SSE), i.e., the error measured in pixels.
-     */
-    geometricError: Number;
+	/**
+	 * The error, in meters, introduced if this tileset is not rendered. At runtime, the geometric error is used to compute screen space error (SSE), i.e., the error measured in pixels.
+	 */
+	geometricError: Number;
 
-    /**
-     * The root tile.
-     */
-    root: TileBase;
+	/**
+	 * The root tile.
+	 */
+	root: TileBase;
 
-    // optional properties
+	// optional properties
 
-    /**
-     * Names of 3D Tiles extensions used somewhere in this tileset.
-     */
-    extensionsUsed?: string[];
+	/**
+	 * Names of 3D Tiles extensions used somewhere in this tileset.
+	 */
+	extensionsUsed?: string[];
 
-    /**
-     * Names of 3D Tiles extensions required to properly load this tileset.
-     */
-    extensionsRequired?: string[];
+	/**
+	 * Names of 3D Tiles extensions required to properly load this tileset.
+	 */
+	extensionsRequired?: string[];
 
-    /**
-     * A dictionary object of metadata about per-feature properties.
-     */
-    properties?: Record<string, any>;
+	/**
+	 * A dictionary object of metadata about per-feature properties.
+	 */
+	properties?: Record<string, any>;
 
-    /**
+	/**
 	 * Dictionary object with extension-specific objects.
 	 */
-    extensions? : Record<string, any>;
+	extensions? : Record<string, any>;
 
-    extras? : Record<string, any>;
+	extras? : Record<string, any>;
 
 }
