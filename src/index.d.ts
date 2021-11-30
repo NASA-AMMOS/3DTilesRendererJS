c1467594ab2f56fc842d11f0a28477bd8efe40d5
Dave Buchhofer <buchhofer@matterport.com>
2021-11-30 11:40:27 -0500

    [index][types] - export new loaders
    Bonus: includes a couple color types that were missing previously

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,48 +1,5 @@
-import {
-	DebugTilesRenderer,
-	NONE,
-	SCREEN_ERROR,
-	GEOMETRIC_ERROR,
-	DISTANCE,
-	DEPTH,
-	RELATIVE_DEPTH,
-	IS_LEAF,
-	RANDOM_COLOR,
-} from './three/DebugTilesRenderer';
-import { TilesRenderer } from './three/TilesRenderer';
-import { TilesGroup } from './three/TilesGroup';
-import { B3DMLoader } from './three/B3DMLoader';
-import { I3DMLoader } from './three/I3DMLoader';
-import { PNTSLoader } from './three/PNTSLoader';
-import { CMPTLoader } from './three/CMPTLoader';
-
-import { TilesRendererBase } from './base/TilesRendererBase';
-import { Tile } from './base/Tile';
-import { TileBase } from './base/TileBase';
-import { Tileset } from './base/Tileset';
-import { B3DMLoaderBase } from './base/B3DMLoaderBase';
-import { I3DMLoaderBase } from './base/I3DMLoaderBase';
-import { PNTSLoaderBase } from './base/PNTSLoaderBase';
-import { CMPTLoaderBase } from './base/CMPTLoaderBase';
-
-import { LRUCache } from './utilities/LRUCache';
-import { PriorityQueue } from './utilities/PriorityQueue';
-
 export {
 	DebugTilesRenderer,
-	TilesRenderer,
-	B3DMLoader,
-
-	Tile,
-	TileBase,
-	Tileset,
-	TilesRendererBase,
-	TilesGroup,
-	B3DMLoaderBase,
-
-	LRUCache,
-	PriorityQueue,
-
 	NONE,
 	SCREEN_ERROR,
 	GEOMETRIC_ERROR,
@@ -51,4 +8,26 @@ export {
 	RELATIVE_DEPTH,
 	IS_LEAF,
 	RANDOM_COLOR,
-};
+	RANDOM_NODE_COLOR,
+	CUSTOM_COLOR,
+} from './three/DebugTilesRenderer';
+export { TilesRenderer } from './three/TilesRenderer';
+export { TilesGroup } from './three/TilesGroup';
+export { B3DMLoader } from './three/B3DMLoader';
+export { I3DMLoader } from './three/I3DMLoader';
+export { PNTSLoader } from './three/PNTSLoader';
+export { CMPTLoader } from './three/CMPTLoader';
+export { GLTFExtensionLoader } from './three/GLTFExtensionLoader';
+
+export { TilesRendererBase } from './base/TilesRendererBase';
+export { Tile } from './base/Tile';
+export { TileBase } from './base/TileBase';
+export { Tileset } from './base/Tileset';
+export { B3DMLoaderBase } from './base/B3DMLoaderBase';
+export { I3DMLoaderBase } from './base/I3DMLoaderBase';
+export { PNTSLoaderBase } from './base/PNTSLoaderBase';
+export { CMPTLoaderBase } from './base/CMPTLoaderBase';
+export { LoaderBase } from './base/LoaderBase';
+
+export { LRUCache } from './utilities/LRUCache';
+export { PriorityQueue } from './utilities/PriorityQueue';
