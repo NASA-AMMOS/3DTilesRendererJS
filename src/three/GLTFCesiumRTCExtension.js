2672393eba652553028cc167e3fa25c5042d0d22
Jesse Small <jesse.small@veerum.com>
2022-11-03 13:39:47 -0600

    Adding additional null check for non-cesium-rtc tiles

--- src/three/GLTFCesiumRTCExtension.js
+++ src/three/GLTFCesiumRTCExtension.js
@@ -8,12 +8,17 @@ export class GLTFCesiumRTCExtension {
 
 	afterRoot( res ) {
 
-		if ( res.parser.json.extensions ) {
+		if ( res.parser.json.extensions && res.parser.json.extensions.CESIUM_RTC ) {
 
 			const { center } = res.parser.json.extensions.CESIUM_RTC;
-			res.scene.position.x += center[ 0 ];
-			res.scene.position.y += center[ 1 ];
-			res.scene.position.z += center[ 2 ];
+
+			if ( center ) {
+
+				res.scene.position.x += center[ 0 ];
+				res.scene.position.y += center[ 1 ];
+				res.scene.position.z += center[ 2 ];
+
+			}
 
 		}
 
