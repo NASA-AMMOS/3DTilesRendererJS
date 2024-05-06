f7f5d9902d071eba05624d0b9bcb2f1344a02f60
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-05-06 18:54:23 +0900

    Remove shadow import

--- example/gltfExample.js
+++ example/gltfExample.js
@@ -4,7 +4,6 @@ import {
 	AmbientLight,
 	WebGLRenderer,
 	PerspectiveCamera,
-	PCFSoftShadowMap,
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
