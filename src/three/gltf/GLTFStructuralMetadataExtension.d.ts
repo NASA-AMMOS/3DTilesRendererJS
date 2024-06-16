3e6a335474b37c1394000d38fffea767fd98fb43
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-06-16 22:16:50 +0900

    Expose more texture info

--- src/three/gltf/GLTFStructuralMetadataExtension.d.ts
+++ src/three/gltf/GLTFStructuralMetadataExtension.d.ts
@@ -8,12 +8,24 @@ export class GLTFStructuralMetadataExtension {
 
 class StructuralMetadata {
 
+	textures: Array<Texture | null>;
+
 	getPropertyTableData( tableIndices: Array<Number>, ids: Array<Number>, target: Array = [] ): Array<any>;
 	getPropertyTableInfo( tableIndices: Array<Number> | null = null ): Array<{ name: String, className: string }>;
 
 	getPropertyTextureData( triangle: Number, barycoord: Vector3, target: Array = [] ): Array<any>;
 	getPropertyTextureDataAsync( triangle: Number, barycoord: Vector3, target: Array = [] ): Promise<Array<any>>;
-	getPropertyTextureInfo(): Array<{ name: String, className: string }>;
+	getPropertyTextureInfo(): Array<{
+		name: String,
+		className: string,
+		properties: {
+			[key]: {
+				index: Number,
+				texCoord: Number,
+				channels: Array<Number>,
+			}
+		}
+	}>;
 
 	getPropertyAttributeData( attributeIndex: Number, target: Array = [] ): Array<any>;
 	getPropertyAttributeInfo(): Array<{ name: String, className: string }>;
