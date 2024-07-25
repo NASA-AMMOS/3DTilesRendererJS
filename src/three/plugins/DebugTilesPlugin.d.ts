f015ed73f6601e273a1b644073025e2a57a42cd2
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-07-25 14:19:06 +0900

    Create DebugTilesPlugin.d.ts

---
+++ src/three/plugins/DebugTilesPlugin.d.ts
@@ -0,0 +1,29 @@
+import { Tile } from '../../base/Tile';
+import { Color } from 'three';
+
+export enum ColorMode {}
+export const NONE : ColorMode;
+export const SCREEN_ERROR : ColorMode;
+export const GEOMETRIC_ERROR : ColorMode;
+export const DISTANCE : ColorMode;
+export const DEPTH : ColorMode;
+export const RELATIVE_DEPTH : ColorMode;
+export const IS_LEAF : ColorMode;
+export const RANDOM_COLOR : ColorMode;
+export const RANDOM_NODE_COLOR: ColorMode;
+export const CUSTOM_COLOR: ColorMode;
+export class DebugTilesPlugin {
+
+	displayBoxBounds : Boolean;
+	displaySphereBounds : Boolean;
+	displayRegionBounds : Boolean;
+	colorMode : ColorMode;
+
+	maxDebugDepth : Number;
+	maxDebugDistance : Number;
+	maxDebugError : Number;
+
+	getDebugColor : ( val: Number, target: Color ) => void;
+	customColorCallback : ( val: Tile, target: Color ) => void;
+
+}
