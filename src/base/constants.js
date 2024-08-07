c945b2743e1610b20a05cb2a3439085b942fc9b2
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-08-07 19:27:52 +0900

    Moon updates (#677)
    * Fix ion support for earth
    
    * Add moon constants
    
    * Add Ellipsoid Tiles Renderer
    
    * Fix load error
    
    * Fix zoomed out position

--- src/base/constants.js
+++ src/base/constants.js
@@ -9,3 +9,7 @@ export const FAILED = 4;
 export const WGS84_RADIUS = 6378137;
 export const WGS84_FLATTENING = 1 / 298.257223563;
 export const WGS84_HEIGHT = - ( WGS84_FLATTENING * WGS84_RADIUS - WGS84_RADIUS );
+
+// https://nssdc.gsfc.nasa.gov/planetary/factsheet/moonfact.html
+export const MOON_HEIGHT = 1736.0 * 1e3;
+export const MOON_RADIUS = 1738.1 * 1e3;
