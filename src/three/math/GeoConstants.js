3a292af16a31394d3e3e2455e8091c6237fc73f3
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-08-07 12:18:08 +0900

    Add moon constants

--- src/three/math/GeoConstants.js
+++ src/three/math/GeoConstants.js
@@ -1,4 +1,5 @@
-import { WGS84_RADIUS, WGS84_HEIGHT } from '../../base/constants.js';
+import { WGS84_RADIUS, WGS84_HEIGHT, MOON_RADIUS, MOON_HEIGHT } from '../../base/constants.js';
 import { Ellipsoid } from './Ellipsoid.js';
 
 export const WGS84_ELLIPSOID = new Ellipsoid( WGS84_RADIUS, WGS84_RADIUS, WGS84_HEIGHT );
+export const MOON_ELLIPSOID = new Ellipsoid( MOON_RADIUS, MOON_RADIUS, MOON_HEIGHT );
