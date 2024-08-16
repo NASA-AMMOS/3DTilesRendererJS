45183d5d66a0e17f039151fb3b244c70f43550ff
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-08-17 01:30:03 +0900

    Update byte estimation

--- src/three/utilities.js
+++ src/three/utilities.js
@@ -1,29 +1,45 @@
 import { estimateBytesUsed as _estimateBytesUsed } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
-import { TextureUtils } from 'three';
+import * as THREE from 'three';
 
-export function estimateBytesUsed( scene ) {
+// Returns the estimated number of bytes used by the object
+export function estimateBytesUsed( object ) {
+
+	// NOTE: This is for backwards compatibility and should be removed later
+	const { TextureUtils } = THREE;
+	if ( ! TextureUtils ) {
+
+		return 0;
+
+	}
+
+	const dedupeSet = new Set();
 
 	let totalBytes = 0;
-	scene.traverse( c => {
+	object.traverse( c => {
 
-		if ( c.geometry ) {
+		// get geometry bytes
+		if ( c.geometry && ! dedupeSet.has( c.geometry ) ) {
 
 			totalBytes += _estimateBytesUsed( c.geometry );
+			dedupeSet.add( c.geometry );
 
 		}
 
+		// get material bytes
 		if ( c.material ) {
 
 			const material = c.material;
 			for ( const key in material ) {
 
 				const value = material[ key ];
-				if ( value && value.isTexture ) {
+				if ( value && value.isTexture && ! dedupeSet.has( value ) ) {
 
 					const { format, type, image } = value;
 					const { width, height } = image;
 					const bytes = TextureUtils.getByteLength( width, height, format, type );
 					totalBytes += value.generateMipmaps ? bytes * 4 / 3 : bytes;
+
+					dedupeSet.add( value );
 
 				}
 
