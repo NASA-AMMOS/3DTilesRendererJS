6be5c9b949af20504cdad69f627e82794d17df46
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-10-12 21:22:04 +0900

    Add BatchedTilesPlugin (#800)
    * Add BatchedTilesPlugin
    
    * Add start to expanding batched mesh
    
    * Split out ExpandingBatchedMesh
    
    * Clean up
    
    * small fixes
    
    * Progress on getting things working
    
    * Check revision
    
    * Texture fixes
    
    * Small fixes
    
    * Some fixes to geometry expansion
    
    * remove extra count
    
    * Fix rendering
    
    * Fix instance deletion
    
    * Remove frustum culling
    
    * Get everything working
    
    * Fix score
    
    * Update raycasting logic
    
    * Add raycasting support
    
    * Update material support
    
    * Add docs
    
    * updates
    
    * Fix disposal
    
    * Handle all already loaded models
    
    * Add comments
    
    * more comments
    
    * fix disposal
    
    * some fixes
    
    * Remove unused fields
    
    * Add model view batched mesh
    
    * Adjust the model view batched mesh to use camera offset
    
    * Temporarily remove mipmaps
    
    * Handle cases where tiles have multiple geometries
    
    * Remove override
    
    * fix use of free ids
    
    * Limit the amount of instances to the size of a 3d texture
    
    * Add stand in options
    
    * Adjust instance count check
    
    * Support multiple geometries per tile
    
    * Handle incongruent attributes, texturs
    
    * Update plugin

---
+++ example/src/plugins/batched/ModelViewBatchedMesh.js
@@ -0,0 +1,103 @@
+import { BatchedMesh, Matrix4, Vector3, Source } from 'three';
+
+const matrix = new Matrix4();
+const vec1 = new Vector3();
+const vec2 = new Vector3();
+export class ModelViewBatchedMesh extends BatchedMesh {
+
+	constructor( ...args ) {
+
+		super( ...args );
+
+		this.resetDistance = 1e4;
+		this._initMatricesTexture();
+		this._matricesTextureHandle = null;
+		this._lastCameraPos = new Matrix4();
+		this._forceUpdate = true;
+
+	}
+
+	_initModelViewMatricesTexture() {
+
+		this._modelViewMatricesTexture = this._matricesTexture.clone();
+		this._modelViewMatricesTexture.source = new Source( {
+			...this._modelViewMatricesTexture.image,
+			data: this._modelViewMatricesTexture.image.data.slice(),
+		} );
+
+	}
+
+	_initMatricesTexture() {
+
+		super._initMatricesTexture();
+		this._initModelViewMatricesTexture();
+
+	}
+
+	setMatrixAt( ...args ) {
+
+		super.setMatrixAt( ...args );
+		this._forceUpdate = true;
+
+	}
+
+	onBeforeRender( renderer, scene, camera, geometry, material, group ) {
+
+		super.onBeforeRender( renderer, scene, camera, geometry, material, group );
+
+		vec1.setFromMatrixPosition( camera.matrixWorld );
+		vec2.setFromMatrixPosition( this._lastCameraPos );
+
+		if ( this._forceUpdate || vec1.distanceTo( vec2 ) > this.resetDistance ) {
+
+			// TODO: resize and re-instantiate the texture here if the size is different
+			const drawInfo = this._drawInfo;
+			const matricesArray = this._matricesTexture.image.data;
+			const modelViewArray = this._modelViewMatricesTexture.image.data;
+			for ( let i = 0; i < drawInfo.length; i ++ ) {
+
+				matrix
+					.fromArray( matricesArray, i * 16 )
+					.premultiply( this.matrixWorld )
+					.premultiply( camera.matrixWorldInverse )
+					.toArray( modelViewArray, i * 16 );
+
+			}
+
+			this._modelViewMatricesTexture.needsUpdate = true;
+			this._lastCameraPos.copy( camera.matrixWorld );
+			this._forceUpdate = false;
+
+		}
+
+		this._matricesTextureHandle = this._matricesTexture;
+		this._matricesTexture = this._modelViewMatricesTexture;
+		this.matrixWorld.copy( this._lastCameraPos );
+
+	}
+
+	onAfterRender() {
+
+		this.updateMatrixWorld();
+		this._matricesTexture = this._matricesTextureHandle;
+		this._matricesTextureHandle = null;
+
+	}
+
+	onAfterShadow( renderer, object, camera, shadowCamera, geometry, depthMaterial/* , group */ ) {
+
+		this.onAfterRender( renderer, null, shadowCamera, geometry, depthMaterial );
+
+	}
+
+	dispose() {
+
+		if ( this._modelViewMatricesTexture ) {
+
+			this._modelViewMatricesTexture.dispose();
+
+		}
+
+	}
+
+}
