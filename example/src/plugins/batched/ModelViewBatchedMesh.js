e9fc01411cbe83ef666d3dc4a1a5a629b39f37ef
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-10-12 09:32:50 +0900

    Adjust the model view batched mesh to use camera offset

--- example/src/plugins/batched/ModelViewBatchedMesh.js
+++ example/src/plugins/batched/ModelViewBatchedMesh.js
@@ -1,13 +1,19 @@
-import { BatchedMesh, Matrix4, Source } from 'three';
+import { BatchedMesh, Matrix4, Vector3, Source } from 'three';
 
 const matrix = new Matrix4();
+const vec1 = new Vector3();
+const vec2 = new Vector3();
 export class ModelViewBatchedMesh extends BatchedMesh {
 
 	constructor( ...args ) {
 
 		super( ...args );
+
+		this.resetDistance = 1e4;
 		this._initMatricesTexture();
 		this._matricesTextureHandle = null;
+		this._lastCameraPos = new Matrix4();
+		this._forceUpdate = true;
 
 	}
 
@@ -28,32 +34,45 @@ export class ModelViewBatchedMesh extends BatchedMesh {
 
 	}
 
+	setMatrixAt( ...args ) {
+
+		super.setMatrixAt( ...args );
+		this._forceUpdate = true;
+
+	}
+
 	onBeforeRender( renderer, scene, camera, geometry, material, group ) {
 
 		super.onBeforeRender( renderer, scene, camera, geometry, material, group );
 
-		// TODO: resize and re-instantiate the texture here if the size is different
+		vec1.setFromMatrixPosition( camera.matrixWorld );
+		vec2.setFromMatrixPosition( this._lastCameraPos );
 
-		// TODO: only update here if the camera matrix has shifted significantly from it's previous
-		// position and apply the delta matrix with the matrix world
-		const drawInfo = this._drawInfo;
-		const matricesArray = this._matricesTexture.image.data;
-		const modelViewArray = this._modelViewMatricesTexture.image.data;
-		for ( let i = 0; i < drawInfo.length; i ++ ) {
+		if ( this._forceUpdate || vec1.distanceTo( vec2 ) > this.resetDistance ) {
 
-			matrix
-				.fromArray( matricesArray, i * 16 )
-				.premultiply( this.matrixWorld )
-				.premultiply( camera.matrixWorldInverse )
-				.toArray( modelViewArray, i * 16 );
+			// TODO: resize and re-instantiate the texture here if the size is different
+			const drawInfo = this._drawInfo;
+			const matricesArray = this._matricesTexture.image.data;
+			const modelViewArray = this._modelViewMatricesTexture.image.data;
+			for ( let i = 0; i < drawInfo.length; i ++ ) {
+
+				matrix
+					.fromArray( matricesArray, i * 16 )
+					.premultiply( this.matrixWorld )
+					.premultiply( camera.matrixWorldInverse )
+					.toArray( modelViewArray, i * 16 );
+
+			}
+
+			this._modelViewMatricesTexture.needsUpdate = true;
+			this._lastCameraPos.copy( camera.matrixWorld );
+			this._forceUpdate = false;
 
 		}
 
-		this._modelViewMatricesTexture.needsUpdate = true;
-
 		this._matricesTextureHandle = this._matricesTexture;
 		this._matricesTexture = this._modelViewMatricesTexture;
-		this.matrixWorld.copy( camera.matrixWorld );
+		this.matrixWorld.copy( this._lastCameraPos );
 
 	}
 
