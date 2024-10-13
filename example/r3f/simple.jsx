8bd537d4e5d12676a9801888d6c9c3800318c5ff
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-10-13 18:32:29 +0900

    Update demo

--- example/r3f/simple.jsx
+++ example/r3f/simple.jsx
@@ -1,69 +1,74 @@
-import { StrictMode } from 'react';
+import { StrictMode, useRef } from 'react';
 import { createRoot } from 'react-dom/client';
-import { TilesPlugin, TilesRenderer, EnvironmentControls } from '../../src/r3f/index';
-import { TilesFadePlugin } from '../src/plugins/fade/TilesFadePlugin';
-
-// R3F + DREI
-import { Canvas } from '@react-three/fiber';
-import {
-	Environment,
-	Grid,
-	GizmoHelper,
-	GizmoViewport
-} from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Environment, GizmoHelper, GizmoViewport, MeshTransmissionMaterial } from '@react-three/drei';
+import { TilesPlugin, TilesRenderer, EnvironmentControls } from '../../src/r3f/index.jsx';
+import { TilesFadePlugin } from '../src/plugins/fade/TilesFadePlugin.js';
 
 const tilesetUrl = 'https://raw.githubusercontent.com/NASA-AMMOS/3DTilesSampleData/master/msl-dingo-gap/0528_0260184_to_s64o256_colorize/0528_0260184_to_s64o256_colorize/0528_0260184_to_s64o256_colorize_tileset.json';
+
+function RotatingMesh() {
+
+	const ref = useRef();
+
+	useFrame( () => {
+
+		const mesh = ref.current;
+		mesh.rotation.x = Math.sin( window.performance.now() * 0.0005 ) * 2;
+		mesh.rotation.y = Math.cos( window.performance.now() * 0.0015 );
+
+	} );
+
+	return <mesh position={ [ 0, 4, - 4 ] } scale={ 2 } ref={ ref }>
+		<icosahedronGeometry />
+		<MeshTransmissionMaterial thickness={ 1.5 } chromaticAberration={ 0.25 } color={ 0x80DEEA } />
+	</mesh>;
+
+}
 
 function App() {
 
 	return (
-		<div id="canvas-container" style={ {
-			width: '100%',
-			height: '100%',
-			position: 'absolute',
-			margin: 0,
-			left: 0,
-			top: 0,
-		} }>
-			<Canvas>
-				{/* 3D Tiles renderer tileset */}
-				<group rotation-x={ Math.PI / 2 }>
-					<TilesRenderer url={ tilesetUrl } lruCache-minSize={ 0 }>
-						<TilesPlugin plugin={ TilesFadePlugin } fadeDuration={ 500 } />
-					</TilesRenderer>
-				</group>
+		<Canvas
+			camera={ {
+				position: [ 12, 7.5, 12 ],
+			} }
+			style={ {
+				width: '100%',
+				height: '100%',
+				position: 'absolute',
+				margin: 0,
+				left: 0,
+				top: 0,
+			} }
+		>
+			{/* 3D Tiles renderer tileset */}
+			<group rotation-x={ Math.PI / 2 }>
+				<TilesRenderer url={ tilesetUrl } lruCache-minSize={ 0 }>
+					<TilesPlugin plugin={ TilesFadePlugin } fadeDuration={ 500 } />
+				</TilesRenderer>
+			</group>
 
-				{/* add mesh to local frame */}
-				<mesh position={ [ 0, 2, 0 ] }>
-					<boxGeometry />
-					<meshStandardMaterial />
-				</mesh>
+			{/* add mesh to local frame */}
+			<RotatingMesh />
 
-				{/* Controls */}
-				<EnvironmentControls enableDamping={ true } />
+			{/* Controls */}
+			<EnvironmentControls enableDamping={ true } />
 
-				{/* other r3f staging */}
-				<Environment
-					preset="sunset" background={ true }
-					backgroundBlurriness={ 0.9 }
-					environmentIntensity={ 1 }
-				/>
-				<Grid
-					infiniteGrid={ true } cellSize={ 1 } sectionSize={ 10 }
-					fadeDistance={ 20000 } fadeStrength={ 50 }
-				/>
-				<GizmoHelper alignment="bottom-right" margin={ [ 80, 80 ] }>
-					<GizmoViewport axisColors={ [ '#9d4b4b', '#2f7f4f', '#3b5b9d' ] } labelColor="white" />
-				</GizmoHelper>
+			{/* other r3f staging */}
+			<Environment
+				preset="sunset" background={ true }
+				backgroundBlurriness={ 0.9 }
+				environmentIntensity={ 1 }
+			/>
+			<GizmoHelper alignment="bottom-right">
+				<GizmoViewport />
+			</GizmoHelper>
 
-			</Canvas>
-		</div>
+		</Canvas>
 	);
 
 }
-
-export default App;
-
 
 createRoot( document.getElementById( 'root' ) ).render(
 	<StrictMode>
