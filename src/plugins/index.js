512c8082d4b4b91ceca2f33ab49e5ab53cab8036
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-12-01 16:27:57 +0900

    Fix imports

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -3,6 +3,8 @@ export { CesiumIonAuthPlugin } from './three/CesiumIonAuthPlugin.js';
 export { GoogleCloudAuthPlugin } from './three/GoogleCloudAuthPlugin.js';
 export { UpdateOnChangePlugin } from './three/UpdateOnChangePlugin.js';
 export { TileCompressionPlugin } from './three/TileCompressionPlugin.js';
+export { GLTFExtensionsPlugin } from './three/GLTFExtensionsPlugin.js';
+export { ReorientationPlugin } from './three/ReorientationPlugin.js';
 export { TilesFadePlugin } from './three/fade/TilesFadePlugin.js';
 export * from './three/DebugTilesPlugin.js';
 
