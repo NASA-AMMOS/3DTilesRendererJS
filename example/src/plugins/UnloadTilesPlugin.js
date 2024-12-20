c4decefa14c5894662de443e64d98e4582cc762a
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-12-20 10:21:38 +0900

    remove comments

--- example/src/plugins/UnloadTilesPlugin.js
+++ example/src/plugins/UnloadTilesPlugin.js
@@ -6,10 +6,6 @@ import { estimateBytesUsed } from '../../../src/three/utilities.js';
 // - abstract the "tile visible" callback so fade tiles can call it when tiles are _actually_ marked as non-visible
 // - add a memory unload function to the tiles renderer that can be called and reacted to by any plugin including BatchedMesh,
 // though this may prevent different options. Something like a subfunction that "disposeTile" calls without full disposal.
-// - For BatchedMeshPlugin
-//    - Needs flag indicating whether tiles should be fully disposed on upload
-//    - Needs to be smarter and reupload if-needed
-//    - Raycasting needs to be smarter and raycast against non-uploaded geometry
 export class UnloadTilesPlugin {
 
 	constructor() {
