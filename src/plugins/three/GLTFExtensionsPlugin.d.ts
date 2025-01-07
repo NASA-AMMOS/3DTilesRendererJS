bc1c20bbf99b028106b9e38591e64b93971f94d8
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2025-01-07 14:33:04 +0100

    fix(GLTFExtensionsPlugin.d.ts): make all parameters optional

--- src/plugins/three/GLTFExtensionsPlugin.d.ts
+++ src/plugins/three/GLTFExtensionsPlugin.d.ts
@@ -5,14 +5,14 @@ import { GLTFLoaderPlugin, GLTFParser } from 'three/examples/jsm/loaders/GLTFLoa
 export class GLTFExtensionsPlugin {
 
 	constructor( options: {
-		metadata: boolean,
-		rtc: boolean,
+		metadata?: boolean,
+		rtc?: boolean,
 
-		plugins: Array<( parser: GLTFParser ) => GLTFLoaderPlugin>,
+		plugins?: Array<( parser: GLTFParser ) => GLTFLoaderPlugin>,
 
-		dracoLoader: DRACOLoader | null,
-		ktxLoader: KTX2Loader | null,
-		autoDispose: boolean,
+		dracoLoader?: DRACOLoader | null,
+		ktxLoader?: KTX2Loader | null,
+		autoDispose?: boolean,
 	} );
 
 }
