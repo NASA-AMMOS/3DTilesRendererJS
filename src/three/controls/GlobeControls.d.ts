13059090023523d5d96454c0bf9757f08fcfe5c6
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2025-01-07 16:22:43 +0100

    fix(controls): export controls declaration files (GlobeControls, EnvironmentControls, CameraTransitionManager)

---
+++ src/three/controls/GlobeControls.d.ts
@@ -0,0 +1,15 @@
+import { Group, Vector3 } from 'three';
+import { Ellipsoid } from '../math/Ellipsoid';
+import { EnvironmentControls } from './EnvironmentControls';
+
+export class GlobeControls extends EnvironmentControls {
+
+	readonly isGlobeControls: true;
+
+	get ellipsoid(): Ellipsoid;
+	get tilesGroup(): Group;
+
+	getVectorToCenter( target: Vector3 ): Vector3;
+	getDistanceToCenter(): number;
+
+}
