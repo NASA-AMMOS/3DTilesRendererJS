efa0fbd535378e8a31298c6068f45b0f76f9357f
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-01-12 22:01:11 +0900

    simplify

--- src/three/TilesRenderer.js
+++ src/three/TilesRenderer.js
@@ -644,24 +644,14 @@ export class TilesRenderer extends TilesRendererBase {
 			case 'glb': {
 
 				const loader = manager.getHandler( 'path.gltf' ) || manager.getHandler( 'path.glb' ) || new GLTFLoader( manager );
+				loader.setWithCredentials( fetchOptions.credentials === 'include' );	
+				loader.setRequestHeader( fetchOptions.headers || {} );
 				if ( fetchOptions.credentials === 'include' && fetchOptions.mode === 'cors' ) {
 
 					loader.setCrossOrigin( 'use-credentials' );
 	
 				}
-	
-				if ( 'credentials' in fetchOptions ) {
-	
-					loader.setWithCredentials( fetchOptions.credentials === 'include' );
-	
-				}
-	
-				if ( fetchOptions.headers ) {
-	
-					loader.setRequestHeader( fetchOptions.headers );
-	
-				}
-	
+
 				// assume any pre-registered loader has paths configured as the user desires, but if we're making
 				// a new loader, use the working path during parse to support relative uris on other hosts
 				let resourcePath = loader.resourcePath || loader.path || workingPath;
