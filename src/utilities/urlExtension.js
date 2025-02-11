36c9781a89dcd754e598e204c724660b25627b09
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-02-11 15:57:46 +0900

    Revert extension extraction change

--- src/utilities/urlExtension.js
+++ src/utilities/urlExtension.js
@@ -5,21 +5,28 @@
  */
 export function getUrlExtension( url ) {
 
-	if ( ! url ) {
+	let parsedUrl;
+	try {
 
+		parsedUrl = new URL( url, 'http://fakehost.com/' );
+
+	} catch ( _ ) {
+
+		// Ignore invalid URLs
 		return null;
 
 	}
 
-	url = url.replace( /\?.*/, '' );
-	const dotIndex = url.lastIndexOf( '.' );
-	if ( dotIndex === - 1 ) {
+	const filename = parsedUrl.pathname.split( '/' ).pop();
+	const dotIndex = filename.lastIndexOf( '.' );
+	if ( dotIndex === - 1 || dotIndex === filename.length - 1 ) {
 
+		// Has no extension or has trailing . character
 		return null;
 
 	}
 
-	const extension = url.substring( dotIndex + 1 );
-	return extension || null;
+	const extension = filename.substring( dotIndex + 1 );
+	return extension;
 
 }
