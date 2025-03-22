70e062e8deca6bc9d91a2462b679b27dbbe686d3
Kayden T <82050470+kayden1940@users.noreply.github.com>
2025-03-22 15:49:41 -0600

    using tsc to test type checking for r3f props, remove unnecessary packages

---
+++ test/types/r3f/typeCheck.test.tsx
@@ -0,0 +1,10 @@
+import { EnvironmentControls } from '../../../src/r3f';
+import React from 'react';
+
+// EnvironmentControls taking valid props, not expect ts error.
+<EnvironmentControls enableDamping={ true } />;
+// EnvironmentControls taking invalid props, expect ts error. tsc test will fail if ts-expect-error directive failed to capture errror.
+//@ts-expect-error
+<EnvironmentControls enableDamping={ "incorrect type" }/>;
+//@ts-expect-error
+<EnvironmentControls foo={"bar"} />;
