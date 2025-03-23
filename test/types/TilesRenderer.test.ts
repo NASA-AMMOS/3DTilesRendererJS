aa329070ccc2204afd3b742b12ea7a413595b7f5
Kayden T <82050470+kayden1940@users.noreply.github.com>
2025-03-23 16:08:13 -0600

    types/react install, remove skipLibCheck

--- test/types/TilesRenderer.test.ts
+++ test/types/TilesRenderer.test.ts
@@ -1,37 +1,46 @@
 import { Camera, Object3D } from 'three';
 import { Tile, TilesRenderer } from '../../src';
+import { expect, test } from "@jest/globals";
 
-function addCamera( event: { camera: Camera } ) {}
-function deleteCamera( event: { camera: Camera } ) {}
-function emptyEvent( event: { } ) {}
-function loadTileset( event: { tileSet: object, url: string } ) {}
-function loadModel( event: { scene: Object3D; tile: Tile } ) {}
-function disposeModel( event: { scene: Object3D; tile: Tile; } ) {}
-function tileVisibilityChange( event: { scene: Object3D; tile: Tile; visible: boolean } ) {}
+function addCamera(event: { camera: Camera }) { }
+function deleteCamera(event: { camera: Camera }) { }
+function emptyEvent(event: {}) { }
+function loadTileset(event: { tileSet: object, url: string }) { }
+function loadModel(event: { scene: Object3D; tile: Tile }) { }
+function disposeModel(event: { scene: Object3D; tile: Tile; }) { }
+function tileVisibilityChange(event: { scene: Object3D; tile: Tile; visible: boolean }) { }
 
-function whatever( event: unknown ) {}
+function whatever(event: unknown) { }
 
 // This function is not meant to be executed, but just here to
 // guarantee that the exported types match what is expected.
 // eslint-disable-next-line no-unused-vars
-function typecheck( renderer: TilesRenderer ) {
+function typecheck(renderer: TilesRenderer) {
 
 	// Check events emitted by the TilesRenderer
-	renderer.addEventListener( 'add-camera', addCamera );
-	renderer.addEventListener( 'delete-camera', deleteCamera );
-	renderer.addEventListener( 'camera-resolution-change', emptyEvent );
-	renderer.addEventListener( 'load-tile-set', loadTileset );
-	renderer.addEventListener( 'tiles-load-start', emptyEvent );
-	renderer.addEventListener( 'tiles-load-end', emptyEvent );
-	renderer.addEventListener( 'load-content', emptyEvent );
-	renderer.addEventListener( 'load-model', loadModel );
-	renderer.addEventListener( 'dispose-model', disposeModel );
-	renderer.addEventListener( 'tile-visibility-change', tileVisibilityChange );
-	renderer.addEventListener( 'update-before', emptyEvent );
-	renderer.addEventListener( 'update-after', emptyEvent );
+	renderer.addEventListener('add-camera', addCamera);
+	renderer.addEventListener('delete-camera', deleteCamera);
+	renderer.addEventListener('camera-resolution-change', emptyEvent);
+	renderer.addEventListener('load-tile-set', loadTileset);
+	renderer.addEventListener('tiles-load-start', emptyEvent);
+	renderer.addEventListener('tiles-load-end', emptyEvent);
+	renderer.addEventListener('load-content', emptyEvent);
+	renderer.addEventListener('load-model', loadModel);
+	renderer.addEventListener('dispose-model', disposeModel);
+	renderer.addEventListener('tile-visibility-change', tileVisibilityChange);
+	renderer.addEventListener('update-before', emptyEvent);
+	renderer.addEventListener('update-after', emptyEvent);
 
 	// Check that we are still able to call an event type that is not
 	// known by the TilesRenderer itself.
-	renderer.addEventListener( 'my-unknown-event', whatever );
+	renderer.addEventListener('my-unknown-event', whatever);
 
 }
+
+test("TilesRenderer should not throw", () => {
+	const renderer = new TilesRenderer("https://raw.githubusercontent.com/NASA-AMMOS/3DTilesSampleData/master/msl-dingo-gap/0528_0260184_to_s64o256_colorize/0528_0260184_to_s64o256_colorize/0528_0260184_to_s64o256_colorize_tileset.json");
+
+	expect(() => {
+			typecheck(renderer);
+	}).not.toThrow();
+});
