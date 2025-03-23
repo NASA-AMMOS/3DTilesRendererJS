e9232f6b2427c7a2bda302010af19ae354b0a826
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-03-23 16:37:59 +0900

    Fix lint errors

--- test/types/r3f/typeCheck.test.tsx
+++ test/types/r3f/typeCheck.test.tsx
@@ -4,7 +4,9 @@ import React from 'react';
 // EnvironmentControls taking valid props, not expect ts error.
 <EnvironmentControls enableDamping={ true } />;
 // EnvironmentControls taking invalid props, expect ts error. tsc test will fail if ts-expect-error directive failed to capture errror.
+
 //@ts-expect-error
-<EnvironmentControls enableDamping={ "incorrect type" }/>;
+<EnvironmentControls enableDamping={ 'incorrect type' }/>;
+
 //@ts-expect-error
-<EnvironmentControls foo={"bar"} />;
+<EnvironmentControls foo={ 'bar' } />;
