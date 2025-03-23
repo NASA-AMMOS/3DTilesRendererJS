83bb1ffd3dee8133641d4c7e24b61e7971719827
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-03-23 17:07:56 +0900

    spelling error

--- test/types/r3f/typeCheck.test.tsx
+++ test/types/r3f/typeCheck.test.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 // EnvironmentControls taking valid props, not expect ts error.
 <EnvironmentControls enableDamping={ true } />;
-// EnvironmentControls taking invalid props, expect ts error. tsc test will fail if ts-expect-error directive failed to capture errror.
 
+// EnvironmentControls taking invalid props, expect ts error. tsc test will fail if ts-expect-error directive failed to capture error.
 //@ts-expect-error
 <EnvironmentControls enableDamping={ 'incorrect type' }/>;
 
