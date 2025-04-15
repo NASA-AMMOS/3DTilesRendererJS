721596c2a5809fe91b27ad2342479a309768d785
Joseph Sullivan <josephs3d@gmail.com>
2025-04-15 15:06:40 -0500

    semicolon

--- src/plugins/base/ImplicitTilingPlugin.js
+++ src/plugins/base/ImplicitTilingPlugin.js
@@ -47,7 +47,7 @@ export class ImplicitTilingPlugin {
 
 			const loader = new SUBTREELoader( tile );
 			loader.workingPath = tile.__basePath;
-			loader.fetchOptions = this.tiles.fetchOptions
+			loader.fetchOptions = this.tiles.fetchOptions;
 			return loader.parse( buffer );
 
 		}
