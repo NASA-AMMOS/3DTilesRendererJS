42b0bcf8da2d07f3b7b208b3a0537312fa620a7e
Brakebein <bruschie@hotmail.com>
2025-04-24 18:59:25 +0200

    enable damping again, move throttle to top

--- example/mapTiles.js
+++ example/mapTiles.js
@@ -10,7 +10,7 @@ import { asyncScheduler, fromEvent, merge, Subject, takeUntil, throttleTime } fr
 
 let controls, scene, renderer;
 let tiles, camera;
-const resize$ = new Subject();
+const render$ = new Subject();
 const unsubscribe$ = new Subject();
 
 const params = {
@@ -44,6 +44,13 @@ function init() {
 	// events
 	onWindowResize();
 	window.addEventListener( 'resize', onWindowResize, false );
+
+	render$
+		.pipe(
+			// throttle to maximum of 50 FPS
+			throttleTime( 20, asyncScheduler, { leading: false, trailing: true } ),
+		)
+		.subscribe( render );
 
 	// gui initialization
 	const gui = new GUI();
@@ -118,7 +125,7 @@ function initTiles() {
 		// create the controls
 		controls = new GlobeControls( scene, camera, renderer.domElement );
 		controls.setTilesRenderer( tiles );
-		controls.enableDamping = false;
+		controls.enableDamping = true;
 		controls.camera.position.set( 0, 0, 1.75 * 1e7 );
 		controls.camera.quaternion.identity();
 		controls.minDistance = 150;
@@ -133,16 +140,11 @@ function initTiles() {
 		fromEvent( controls, 'end' ),
 		fromEvent( tiles, 'load-content' ),
 		fromEvent( tiles, 'force-rerender' ),
-		resize$,
 	)
-		.pipe(
-			takeUntil( unsubscribe$ ),
-			// throttle to maximum of 50 FPS
-			throttleTime( 20, asyncScheduler, { leading: false, trailing: true } ),
-		)
+		.pipe( takeUntil( unsubscribe$ ) )
 		.subscribe( () => {
 
-			render();
+			render$.next();
 
 		} );
 
@@ -165,7 +167,7 @@ function onWindowResize() {
 
 	renderer.setSize( window.innerWidth, window.innerHeight );
 
-	resize$.next();
+	render$.next();
 
 }
 
