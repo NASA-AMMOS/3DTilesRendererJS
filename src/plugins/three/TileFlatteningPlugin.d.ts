a7ca329ac16b1efd2c68778b1909f7dece6aef70
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-04-24 23:28:48 -0700

    Fix types

--- src/plugins/three/TileFlatteningPlugin.d.ts
+++ src/plugins/three/TileFlatteningPlugin.d.ts
@@ -5,7 +5,7 @@ export class TileFlatteningPlugin {
 	hasShape( mesh: Mesh ): boolean;
 	addShape( mesh: Mesh, direction: Vector3, threshold: number ): void;
 	updateShape( mesh: Mesh ): void;
-	deleteShape( mesh ): boolean;
+	deleteShape( mesh: Mesh ): boolean;
 	clearShapes(): void;
 
 }
