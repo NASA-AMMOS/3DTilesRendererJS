980b49938f88d9548c3b47ecdf265acceb58905b
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-07-27 22:50:23 +0900

    Allow for post-option-assignment

--- src/three/plugins/images/sources/WMTSImageSource.js
+++ src/three/plugins/images/sources/WMTSImageSource.js
@@ -1,11 +1,6 @@
 import { TiledImageSource } from './TiledImageSource.js';
 import { ProjectionScheme } from '../utils/ProjectionScheme.js';
 
-// TODO
-// - WMTS supports tile ranges at the matrix level that need to be accounted for in the tiling scheme
-// - Enable image overlays to work more smoothly when the url is constructed by the image source
-// - Add ability to set wmts fields after the fact?
-// - Account for resolution / scale denominator relative to the equator (implicitly convert to lat / lons)
 export class WMTSImageSource extends TiledImageSource {
 
 	constructor( options = {} ) {
@@ -13,49 +8,15 @@ export class WMTSImageSource extends TiledImageSource {
 		super();
 
 		const {
-			capabilities,
-			dimensions = {},
-		} = options;
-
-		let {
+			capabilities = null,
 			layer = null,
 			tileMatrixSet = null,
 			style = null,
 			url = null,
+			dimensions = {},
 		} = options;
 
-		if ( ! layer ) {
-
-			layer = capabilities.layers[ 0 ];
-
-		} else if ( typeof layer === 'string' ) {
-
-			layer = capabilities.layers.find( l => l.identifier === layer );
-
-		}
-
-		if ( ! tileMatrixSet ) {
-
-			tileMatrixSet = layer.tileMatrixSets[ 0 ];
-
-		} else if ( typeof tileMatrixSet === 'string' ) {
-
-			tileMatrixSet = layer.tileMatrixSets.find( tms => tms.identifier === tileMatrixSet );
-
-		}
-
-		if ( ! style ) {
-
-			style = layer.styles.find( style => style.isDefault ).identifier;
-
-		}
-
-		if ( ! url ) {
-
-			url = layer.resourceUrls[ 0 ].template;
-
-		}
-
+		this.capabilities = capabilities;
 		this.layer = layer;
 		this.tileMatrixSet = tileMatrixSet;
 		this.style = style;
@@ -75,15 +36,52 @@ export class WMTSImageSource extends TiledImageSource {
 
 	init() {
 
-		// transform the url
-		const { tiling, layer, tileMatrixSet, style, url, dimensions } = this;
+		const { tiling, dimensions, capabilities } = this;
+		let { layer, tileMatrixSet, style, url } = this;
 
+		// extract the layer to use
+		if ( ! layer ) {
+
+			layer = capabilities.layers[ 0 ];
+
+		} else if ( typeof layer === 'string' ) {
+
+			layer = capabilities.layers.find( l => l.identifier === layer );
+
+		}
+
+		// extract the tile matrix set
+		if ( ! tileMatrixSet ) {
+
+			tileMatrixSet = layer.tileMatrixSets[ 0 ];
+
+		} else if ( typeof tileMatrixSet === 'string' ) {
+
+			tileMatrixSet = layer.tileMatrixSets.find( tms => tms.identifier === tileMatrixSet );
+
+		}
+
+		// extract the style
+		if ( ! style ) {
+
+			style = layer.styles.find( style => style.isDefault ).identifier;
+
+		}
+
+		// extract the url template
+		if ( ! url ) {
+
+			url = layer.resourceUrls[ 0 ].template;
+
+		}
+
+		// determine the projection
 		const projection = tileMatrixSet.supportedCRS.includes( '4326' ) ? 'EPSG:4326' : 'EPSG:3857';
 
+		// generate the tiling scheme
 		tiling.flipY = true;
 		tiling.setProjection( new ProjectionScheme( projection ) );
 		tiling.setBounds( ...tiling.projection.getBounds() );
-
 		tileMatrixSet.tileMatrices.forEach( ( tm, i ) => {
 
 			const { tileWidth, tileHeight } = tm;
@@ -96,21 +94,25 @@ export class WMTSImageSource extends TiledImageSource {
 
 		} );
 
-		this.url = url
+		// construct the url
+		url = url
 			.replace( /{\s*TileMatrixSet\s*}/g, tileMatrixSet.identifier )
 			.replace( /{\s*Style\s*}/g, style );
 
+		// fill in the dimension values
 		for ( const key in dimensions ) {
 
-			this.url = this.url.replace( new RegExp( `{\\s*${ key }\\s*}` ), dimensions[ key ] );
+			url = url.replace( new RegExp( `{\\s*${ key }\\s*}` ), dimensions[ key ] );
 
 		}
 
 		layer.dimensions.forEach( dim => {
 
-			this.url = this.url.replace( new RegExp( `{\\s*${ dim.identifier }\\s*}` ), dim.defaultValue );
+			url = url.replace( new RegExp( `{\\s*${ dim.identifier }\\s*}` ), dim.defaultValue );
 
 		} );
+
+		this.url = url;
 
 		return Promise.resolve();
 
