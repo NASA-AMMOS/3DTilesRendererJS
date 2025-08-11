c545fdc8df5a2b22b3edfb4d2f62ca4593aab8ec
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-08-11 11:55:18 +0900

    Bundler: Add build step for exports (#1254)
    * Add bundle step
    
    * Fix build
    
    * Add potential approach for examples
    
    * Add alias support
    
    * Add prepublish script
    
    * Add test case for using the build dependencies
    
    * Update dev setup information.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,30 @@
 import { searchForWorkspaceRoot, loadEnv } from 'vite';
 import fs from 'fs';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default ( { mode } ) => {
 
 	process.env = { ...process.env, ...loadEnv( mode, process.cwd() ) };
 
+	const useBuild = mode === 'use-build';
+	const aliases = {
+		'3d-tiles-renderer/r3f': path.resolve( './src/r3f/index.jsx' ),
+		'3d-tiles-renderer/core': path.resolve( './src/core/renderer/index.js' ),
+		'3d-tiles-renderer/core/plugins': path.resolve( './src/core/plugins/index.js' ),
+		'3d-tiles-renderer/three/core': path.resolve( './src/three/renderer/index.js' ),
+		'3d-tiles-renderer/three/plugins': path.resolve( './src/three/plugins/index.js' ),
+		'3d-tiles-renderer/plugins': path.resolve( './src/plugins.js' ),
+		'3d-tiles-renderer': path.resolve( './src/index.js' ),
+	};
+
 	return {
 		root: './example/',
 		envDir: '.',
 		base: '',
+		resolve: {
+			alias: useBuild ? null : aliases,
+		},
 		build: {
 			sourcemap: true,
 			outDir: './bundle/',
