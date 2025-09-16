2338d00d3181c76a93712d4f0c463ce5e2c68f49
Xavier Willemin <xavier@slashnet.ch>
2025-09-16 14:06:02 +0200

    Allow LoadRegion to be masked (#1261)
    * Allow LoadRegion to be masked
    
    * Update inView API (breaking)
    
    * Revert raycasting override
    
    * Update "mask" logic
    
    * Add missing parameter
    
    * Fix demo
    
    * Cleanup, change region constructors to use object
    
    * Remove comment
    
    * Docs update
    
    ---------
    
    Co-authored-by: Garrett Johnson <garrett.kjohnson@gmail.com>

--- src/three/plugins/LoadRegionPlugin.js
+++ src/three/plugins/LoadRegionPlugin.js
@@ -50,27 +50,41 @@ export class LoadRegionPlugin {
 
 	}
 
+	// Calculates shape intersections and associated error values to use. If "mask" shapes are present then
+	// tiles are only loaded if they are within those shapes.
 	calculateTileViewError( tile, target ) {
 
 		const boundingVolume = tile.cached.boundingVolume;
 		const { regions, tiles } = this;
 
-		let visible = false;
+		let inShape = false;
+		let inMask = null;
 		let maxError = - Infinity;
 		for ( const region of regions ) {
 
+			// Check if the tile is intersecting the shape and calculate the
+			// view and error values.
 			const intersects = region.intersectsTile( boundingVolume, tile, tiles );
-			if ( intersects ) {
+			inShape = inShape || intersects;
+			maxError = Math.max( region.calculateError( tile, tiles ), maxError );
 
-				visible = true;
-				maxError = Math.max( region.calculateError( tile, tiles ), maxError );
+			// Store whether the tile is in a "mask" shape if they exist. If "inMask" is
+			// still "null" by the end of the loop then there are no mask elements.
+			if ( region.mask ) {
+
+				inMask = inMask || intersects;
 
 			}
 
 		}
 
-		target.inView = visible;
+		// A tile should only be visible if it intersects a shape and a mask shape or there
+		// are no masks.
+		target.inView = inShape && inMask !== false;
 		target.error = maxError;
+
+		// Returning "false" indicates "no operation" and all results will be ignored.
+		return target.inView || inMask !== null;
 
 	}
 
@@ -85,9 +99,22 @@ export class LoadRegionPlugin {
 // Definitions of predefined regions
 export class BaseRegion {
 
-	constructor( errorTarget = 10 ) {
+	constructor( options = {} ) {
+
+		if ( typeof options === 'number' ) {
+
+			console.warn( 'LoadRegionPlugin: Region constructor has been changed to take options as an object.' );
+			options = { errorTarget: options };
+
+		}
+
+		const {
+			errorTarget = 10,
+			mask = false,
+		} = options;
 
 		this.errorTarget = errorTarget;
+		this.mask = mask;
 
 	}
 
@@ -103,9 +130,21 @@ export class BaseRegion {
 
 export class SphereRegion extends BaseRegion {
 
-	constructor( errorTarget = 10, sphere = new Sphere() ) {
+	constructor( options = {} ) {
 
-		super( errorTarget );
+		if ( typeof options === 'number' ) {
+
+			console.warn( 'SphereRegion: Region constructor has been changed to take options as an object.' );
+			options = {
+				errorTarget: arguments[ 0 ],
+				sphere: arguments[ 1 ],
+			};
+
+		}
+
+		const { sphere = new Sphere() } = options;
+
+		super( options );
 		this.sphere = sphere.clone();
 
 	}
@@ -120,9 +159,21 @@ export class SphereRegion extends BaseRegion {
 
 export class RayRegion extends BaseRegion {
 
-	constructor( errorTarget = 10, ray = new Ray() ) {
+	constructor( options = {} ) {
 
-		super( errorTarget );
+		if ( typeof options === 'number' ) {
+
+			console.warn( 'RayRegion: Region constructor has been changed to take options as an object.' );
+			options = {
+				errorTarget: arguments[ 0 ],
+				ray: arguments[ 1 ],
+			};
+
+		}
+
+		const { ray = new Ray() } = options;
+
+		super( options );
 		this.ray = ray.clone();
 
 	}
@@ -137,9 +188,21 @@ export class RayRegion extends BaseRegion {
 
 export class OBBRegion extends BaseRegion {
 
-	constructor( errorTarget = 10, obb = new OBB() ) {
+	constructor( options = {} ) {
 
-		super( errorTarget );
+		if ( typeof options === 'number' ) {
+
+			console.warn( 'RayRegion: Region constructor has been changed to take options as an object.' );
+			options = {
+				errorTarget: arguments[ 0 ],
+				obb: arguments[ 1 ],
+			};
+
+		}
+
+		const { obb = new OBB() } = options;
+
+		super( options );
 		this.obb = obb.clone();
 		this.obb.update();
 
