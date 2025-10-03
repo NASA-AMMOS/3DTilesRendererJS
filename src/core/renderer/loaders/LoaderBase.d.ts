a79f874b5d1e1c49d2ce8017c39b115d8c4f4587
Xavier Willemin <xavier@slashnet.ch>
2025-10-03 02:45:51 +0200

    Fix various typings (#1324)
    * Add TilesRenderer cameras in typings
    
    * Add missing typings in Ellipsoid
    
    * Improve DebugTilesPlugin typings
    
    * Fix WMTSTilesOverlay typings (aligned with WMTSTilesPlugin)
    
    * Deprecate load in favor of loadAsync in LoaderBase typings
    
    * WMTSTilesOverlay: url is optional

--- src/core/renderer/loaders/LoaderBase.d.ts
+++ src/core/renderer/loaders/LoaderBase.d.ts
@@ -2,7 +2,9 @@ export class LoaderBase<Result = any, ParseResult = Promise< Result >> {
 
 	fetchOptions: any;
 	workingPath: string;
+	/** @deprecated in favor of `loadAsync` */
 	load( url: string ): Promise< Result >;
+	loadAsync( url: string ): Promise< Result >;
 	resolveExternalURL( url: string ): string;
 	parse( buffer: ArrayBuffer ): ParseResult;
 
