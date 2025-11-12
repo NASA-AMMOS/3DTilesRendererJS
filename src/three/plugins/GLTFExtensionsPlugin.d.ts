1cf73beae4e19dd82620ac72959173e0c9cab8b2
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2025-11-12 10:37:22 +0100

    GLTFExtensionsPlugin: update typing

--- src/three/plugins/GLTFExtensionsPlugin.d.ts
+++ src/three/plugins/GLTFExtensionsPlugin.d.ts
@@ -1,5 +1,6 @@
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader.js';
+import { MeshoptDecoder } from 'three/examples/jsm/libs/meshopt_decoder.module.js';
 import { GLTFLoaderPlugin, GLTFParser } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 export class GLTFExtensionsPlugin {
@@ -12,6 +13,7 @@ export class GLTFExtensionsPlugin {
 
 		dracoLoader?: DRACOLoader | null,
 		ktxLoader?: KTX2Loader | null,
+		meshoptDecoder?: typeof MeshoptDecoder | null,
 		autoDispose?: boolean,
 	} );
 
