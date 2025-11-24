17f09f62fb59593f9436b21800dfaafbb70be42d
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-11-24 17:14:13 +0900

    README update

--- src/plugins.js
+++ src/plugins.js
@@ -1,2 +1,11 @@
-export * from '3d-tiles-renderer/core/plugins';
+export {
+	// excluded to avoid conflicts. Remove once the deprecated three.js versions are removed
+	// CesiumIonAuthPlugin,
+	// GoogleCloudAuthPlugin,
+	ImplicitTilingPlugin,
+	EnforceNonZeroErrorPlugin,
+	GoogleCloudAuth,
+	CesiumIonAuth,
+	QuantizedMeshLoaderBase,
+} from '3d-tiles-renderer/core/plugins';
 export * from '3d-tiles-renderer/three/plugins';
