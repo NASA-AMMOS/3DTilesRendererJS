9125bea7884af65554bb1db564dd2a8eabcf6fd8
Brakebein <bruschie@hotmail.com>
2025-12-07 02:34:15 +0100

    Adds support for external imagery providers in CesiumIonOverlay (#1385)
    * Adds support for external imagery providers
    
    Extends CesiumIonOverlay to support external imagery providers like Google Maps and Bing Maps, fetching metadata and adjusting tile URL structures accordingly.
    
    Updates the example to showcase this new feature by adding base map options for different map providers.
    
    * update ImageOverlayPlugin based on PR review
    
    * Small cleanup
    
    * Fix lint
    
    * Separate quad key file
    
    ---------
    
    Co-authored-by: Garrett Johnson <garrett.kjohnson@gmail.com>

--- src/three/plugins/images/ImageOverlayPlugin.js
+++ src/three/plugins/images/ImageOverlayPlugin.js
@@ -3,6 +3,7 @@ import { PriorityQueue } from '3d-tiles-renderer/core';
 import { CesiumIonAuth, GoogleCloudAuth } from '3d-tiles-renderer/core/plugins';
 import { TiledTextureComposer } from './overlays/TiledTextureComposer.js';
 import { XYZImageSource } from './sources/XYZImageSource.js';
+import { QuadKeyImageSource } from './sources/QuadKeyImageSource.js';
 import { TMSImageSource } from './sources/TMSImageSource.js';
 import { forEachTileInBounds, getMeshesCartographicRange, getMeshesPlanarRange } from './overlays/utils.js';
 import { wrapOverlaysMaterial } from './overlays/wrapOverlaysMaterial.js';
@@ -969,14 +970,19 @@ export class ImageOverlayPlugin {
 
 		if ( ! overlay.isInitialized ) {
 
-			overlay.imageSource.fetchData = ( ...args ) => tiles
-				.downloadQueue
-				.add( { priority: - performance.now() }, () => {
-
-					return overlay.fetch( ...args );
-
-				} );
 			overlay.init();
+
+			overlay.whenReady().then( () => {
+
+				overlay.imageSource.fetchData = ( ...args ) => tiles
+					.downloadQueue
+					.add( { priority: - performance.now() }, () => {
+
+						return overlay.fetch( ...args );
+
+					} );
+
+			} );
 
 		}
 
@@ -1622,13 +1628,14 @@ export class CesiumIonOverlay extends ImageOverlay {
 		super( options );
 
 		const { apiToken, autoRefreshToken, assetId } = options;
+		this.options = options;
 		this.assetId = assetId;
 		this.auth = new CesiumIonAuth( { apiToken, autoRefreshToken } );
-		this.imageSource = new TMSImageSource( options );
 
 		this.auth.authURL = `https://api.cesium.com/v1/assets/${ assetId }/endpoint`;
-		this.imageSource.fetchData = ( ...args ) => this.fetch( ...args );
 		this._attributions = [];
+
+		this.externalType = false;
 
 	}
 
@@ -1637,7 +1644,7 @@ export class CesiumIonOverlay extends ImageOverlay {
 		this._whenReady = this
 			.auth
 			.refreshToken()
-			.then( json => {
+			.then( async ( json ) => {
 
 				this._attributions = json.attributions.map( att => ( {
 					value: att.html,
@@ -1645,7 +1652,61 @@ export class CesiumIonOverlay extends ImageOverlay {
 					collapsible: att.collapsible,
 				} ) );
 
-				this.imageSource.url = json.url;
+				if ( json.type !== 'IMAGERY' ) {
+
+					throw new Error( 'CesiumIonOverlay: Only IMAGERY is supported as overlay type.' );
+
+				}
+
+				this.externalType = Boolean( json.externalType );
+
+				switch ( json.externalType ) {
+
+					case 'GOOGLE_2D_MAPS': {
+
+						const { url, session, key, tileWidth } = json.options;
+						const xyzUrl = `${ url }/v1/2dtiles/{z}/{x}/{y}?session=${ session }&key=${ key }`;
+						this.imageSource = new XYZImageSource( {
+							...this.options,
+							url: xyzUrl,
+							tileDimension: tileWidth,
+
+							// Google maps tiles have a fixed depth of 22
+							// https://developers.google.com/maps/documentation/tile/2d-tiles-overview
+							levels: 22,
+						} );
+						break;
+
+					}
+
+					case 'BING': {
+
+						const { url, mapStyle, key } = json.options;
+						const metadataUrl = `${ url }/REST/v1/Imagery/Metadata/${ mapStyle }?incl=ImageryProviders&key=${ key }&uriScheme=https`;
+						const response = await fetch( metadataUrl ).then( res => res.json() );
+						const metadata = response.resourceSets[ 0 ].resources[ 0 ];
+
+						this.imageSource = new QuadKeyImageSource( {
+							...this.options,
+							url: metadata.imageUrl,
+							subdomains: metadata.imageUrlSubdomains,
+							tileDimension: metadata.tileWidth,
+							levels: metadata.zoomMax,
+						} );
+						break;
+
+					}
+
+					default:
+						this.imageSource = new TMSImageSource( {
+							...this.options,
+							url: json.url,
+						} );
+
+				}
+
+				this.imageSource.fetchData = ( ...args ) => this.fetch( ...args );
+
 				return this.imageSource.init();
 
 			} );
@@ -1656,7 +1717,8 @@ export class CesiumIonOverlay extends ImageOverlay {
 
 	fetch( ...args ) {
 
-		return this.auth.fetch( ...args );
+		// bypass auth fetch if asset is external type to prevent CORS error due to wrong bearer token
+		return this.externalType ? super.fetch( ...args ) : this.auth.fetch( ...args );
 
 	}
 
