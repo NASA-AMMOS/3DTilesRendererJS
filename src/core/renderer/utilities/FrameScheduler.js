0cf8ae523f73535709c16aca2589c3993aee9bc3
Bruno Fanini <phoenixbf@gmail.com>
2026-01-10 21:48:21 +0100

    Add pending AF management

--- src/core/renderer/utilities/FrameScheduler.js
+++ src/core/renderer/utilities/FrameScheduler.js
@@ -5,36 +5,122 @@ class FrameScheduler {
 		// XR session
 		this.xrsession = null;
 
+		// Pending AFs
+		this.pending = new Map();
+
 	}
 
 	// Set active XR session
+	// To be called when entering XR
 	setXRSession( xrsession ) {
 
 		this.xrsession = xrsession;
 
+		this.flushPending();
+
 	}
 
+	// Remove XR session (exit)
+	// To be called when exiting XR
 	removeXRSession() {
 
 		this.xrsession = null;
+
+		this.flushPending();
 
 	}
 
 	// Request animation frame (defer to XR session if present)
 	requestAnimationFrame( cb ) {
 
-		if ( ! this.xrsession ) return window.requestAnimationFrame( cb );
+		let handle;
 
-		return this.xrsession.requestAnimationFrame( cb );
+		// Standard AF
+		if ( ! this.xrsession ) {
 
-	};
+			handle = window.requestAnimationFrame( ()=>{
 
-	// Cancel animation frame via handle (defer to XR session if present)
+				this.pending.delete( handle );
+
+				cb();
+
+			} );
+
+			this.pending.set( handle, cb );
+
+			return handle;
+
+		}
+		// XR session
+		else {
+
+			handle = this.xrsession.requestAnimationFrame( ()=>{
+
+				this.pending.delete( handle );
+
+				cb();
+
+			} );
+
+			this.pending.set( handle, cb );
+
+			return handle;
+
+		}
+
+	}
+
+	// Cancel animation frame via handle (defer to XR session if active)
 	cancelAnimationFrame( handle ) {
 
-		if ( ! this.xrsession ) window.cancelAnimationFrame( handle );
+    	this.pending.delete( handle );
 
-		else this.xrsession.cancelAnimationFrame( handle );
+		if ( ! this.xrsession ) {
+
+			window.cancelAnimationFrame( handle );
+
+		}
+
+		else {
+
+			this.xrsession.cancelAnimationFrame( handle );
+
+		}
+
+	}
+
+	// Cancel all pending AFs
+	cancelAllPending() {
+
+		this.pending.forEach( ( cb, handle )=>{
+
+    		this.cancelAnimationFrame( handle );
+
+		} );
+
+	}
+
+	// Flush and complete pending AFs (defer to XR session if active)
+	flushPending() {
+
+		this.pending.forEach( ( cb, handle )=>{
+
+			if ( this.xrsession ) {
+
+				this.xrsession.cancelAnimationFrame( handle );
+
+				cb();
+
+			}
+			else {
+
+				window.cancelAnimationFrame( handle );
+
+				cb();
+
+			}
+
+		} );
 
 	}
 
