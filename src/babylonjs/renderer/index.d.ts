7d728e745428bc092e54ad92fe57956c99e40250
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-03-06 00:46:23 +0900

    Update babylon types

--- src/babylonjs/renderer/index.d.ts
+++ src/babylonjs/renderer/index.d.ts
@@ -1,8 +1,8 @@
-import { TilesRendererBase } from '3d-tiles-renderer/core';
+import { TilesRendererBase, TilesRendererBaseEventMap } from '3d-tiles-renderer/core';
 import { Scene } from '@babylonjs/core/scene';
 import { TransformNode } from '@babylonjs/core/Meshes/transformNode';
 
-export class TilesRenderer extends TilesRendererBase {
+export class TilesRenderer extends TilesRendererBase<TilesRendererBaseEventMap<TransformNode>> {
 
 	group: TransformNode;
 	checkCollisions: boolean;
