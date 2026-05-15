cafdcd4638ab50b9173537f7e089f79bb431e1ff
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-05-15 16:54:49 +0900

    Read PMTiles header

--- src/three/plugins/images/sources/PMTilesImageSource.js
+++ src/three/plugins/images/sources/PMTilesImageSource.js
@@ -2,31 +2,51 @@ import { MVTContentCache, MVTImageSource } from './MVTImageSource.js';
 import { ProjectionScheme } from '../utils/ProjectionScheme.js';
 import { PMTiles } from 'pmtiles';
 
+const DEG2RAD = Math.PI / 180;
+
 class PMTilesContentCache extends MVTContentCache {
 
 	constructor( options = {} ) {
 
 		super( options );
-		this._pmtiles = null;
+		this.instance = null;
 
 	}
 
 	async init() {
 
-		this._pmtiles = new PMTiles( this.url );
-		const header = await this._pmtiles.getHeader();
-		this.tiling.flipY = true;
-		this.tiling.setProjection( new ProjectionScheme( 'EPSG:3857' ) );
-		this.tiling.generateLevels( header.maxZoom, this.tiling.projection.tileCountX, this.tiling.projection.tileCountY, {
-			tilePixelWidth: this.tileDimension,
-			tilePixelHeight: this.tileDimension,
+		const { tiling, tileDimension } = this;
+
+		this.instance = new PMTiles( this.url );
+
+		const header = await this.instance.getHeader();
+		if ( header.tileType !== 1 ) {
+
+			throw new Error( `PMTilesContentCache: expected MVT tile type (1), got ${ header.tileType }` );
+
+		}
+
+		const projection = new ProjectionScheme( 'EPSG:3857' );
+
+		tiling.flipY = true;
+		tiling.setProjection( projection );
+		tiling.setContentBounds(
+			DEG2RAD * header.minLon,
+			DEG2RAD * header.minLat,
+			DEG2RAD * header.maxLon,
+			DEG2RAD * header.maxLat,
+		);
+		tiling.generateLevels( header.maxZoom + 1, projection.tileCountX, projection.tileCountY, {
+			tilePixelWidth: tileDimension,
+			tilePixelHeight: tileDimension,
+			minLevel: header.minZoom,
 		} );
 
 	}
 
 	async fetchTileBuffer( z, x, y, signal ) {
 
-		const res = await this._pmtiles.getZxy( z, x, y, signal );
+		const res = await this.instance.getZxy( z, x, y, signal );
 		return res ? res.data : null;
 
 	}
