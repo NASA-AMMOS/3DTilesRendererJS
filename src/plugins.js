c3ca664d8e242d282ed7bfddf93b52151135bea9
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-05-25 22:00:28 +0900

    Fixes

--- src/plugins.js
+++ src/plugins.js
@@ -3,3 +3,6 @@ export * from '3d-tiles-renderer/core/plugins';
 
 // Export all three.js plugins
 export * from '3d-tiles-renderer/three/plugins';
+
+// De-conflict duplicate export
+export { CesiumIonAuthPlugin } from '3d-tiles-renderer/three/plugins';
