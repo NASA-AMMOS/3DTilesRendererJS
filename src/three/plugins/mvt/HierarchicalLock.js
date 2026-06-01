7e3c64b00387123d4b0ce23254f858b7c95f79a5
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-06-01 13:55:42 +0900

    Updates

--- src/three/plugins/mvt/HierarchicalLock.js
+++ src/three/plugins/mvt/HierarchicalLock.js
@@ -6,6 +6,11 @@ const getKey = ( x, y, l ) => {
 
 };
 
+// "available" indicates that a tile should be loaded and available
+// "active" indicates the given tile is visible and should be displayed, overriding all parents
+// If a single child is marked as active then all siblings are, as well, to prevent gaps
+// TODO: This may need to handle async operations or perhaps a separate data structure should be used for
+// "available" vs "active"?
 export class HierarchicalLock extends EventDispatcher {
 
 	constructor() {
@@ -16,17 +21,55 @@ export class HierarchicalLock extends EventDispatcher {
 
 	}
 
+	markActive( x, y, level ) {
+
+		const bx = Math.floor( x / 2 ) * 2;
+		const by = Math.floor( y / 2 ) * 2;
+
+		this._accrueActive( bx + 0, by + 0, level, true );
+		this._accrueActive( bx + 1, by + 0, level, true );
+		this._accrueActive( bx + 0, by + 1, level, true );
+		this._accrueActive( bx + 1, by + 1, level, true );
+
+	}
+
+	markInactive( x, y, level ) {
+
+		const bx = Math.floor( x / 2 ) * 2;
+		const by = Math.floor( y / 2 ) * 2;
+
+		this._accrueActive( bx + 0, by + 0, level, false );
+		this._accrueActive( bx + 1, by + 0, level, false );
+		this._accrueActive( bx + 0, by + 1, level, false );
+		this._accrueActive( bx + 1, by + 1, level, false );
+
+	}
+
 	lock( x, y, level ) {
 
-		this._accrueLock( x, y, level, true );
+		const bx = Math.floor( x / 2 ) * 2;
+		const by = Math.floor( y / 2 ) * 2;
+
+		this._accrueAvailable( bx + 0, by + 0, level, true );
+		this._accrueAvailable( bx + 1, by + 0, level, true );
+		this._accrueAvailable( bx + 0, by + 1, level, true );
+		this._accrueAvailable( bx + 1, by + 1, level, true );
 
 	}
 
 	unlock( x, y, level ) {
 
-		this._accrueLock( x, y, level, false );
+		const bx = Math.floor( x / 2 ) * 2;
+		const by = Math.floor( y / 2 ) * 2;
+
+		this._accrueAvailable( bx + 0, by + 0, level, false );
+		this._accrueAvailable( bx + 1, by + 0, level, false );
+		this._accrueAvailable( bx + 0, by + 1, level, false );
+		this._accrueAvailable( bx + 1, by + 1, level, false );
 
 	}
+
+	//
 
 	_initLock( x, y, l ) {
 
@@ -38,9 +81,11 @@ export class HierarchicalLock extends EventDispatcher {
 				x,
 				y,
 				level: l,
+				present: 0,
 				ref: 0,
 				override: 0,
-				dispatched: false,
+				activeDispatched: false,
+				presentDispatched: false,
 			};
 
 		}
@@ -76,18 +121,29 @@ export class HierarchicalLock extends EventDispatcher {
 		const { locks } = this;
 		const lock = locks[ key ];
 		const active = lock.ref > 0 && lock.override === 0;
-		if ( active !== lock.dispatched ) {
+		if ( active !== lock.activeDispatched ) {
 
 			const { x, y, level } = lock;
-			lock.dispatched = true;
+			lock.activeDispatched = active;
 			this.dispatchEvent( {
-				type: 'toggle',
+				type: 'active-toggle',
 				active, x, y, level,
 			} );
 
 		}
 
-		if ( lock.ref === 0 && lock.override === 0 ) {
+		if ( Boolean( lock.present ) !== lock.presentDispatched ) {
+
+			const { x, y, level } = lock;
+			lock.activeDispatched = lock.present;
+			this.dispatchEvent( {
+				type: 'present-toggle',
+				active, x, y, level,
+			} );
+
+		}
+
+		if ( lock.ref === 0 && lock.override === 0 && lock.present === 0 ) {
 
 			delete locks[ key ];
 
@@ -95,7 +151,7 @@ export class HierarchicalLock extends EventDispatcher {
 
 	}
 
-	_accrueLock( x, y, level, incr ) {
+	_accrueActive( x, y, level, incr ) {
 
 		let key = getKey( x, y, level );
 		this._initLock( x, y, level );
@@ -114,6 +170,17 @@ export class HierarchicalLock extends EventDispatcher {
 			this._resolveEvents( key );
 
 		}
+
+	}
+
+	_accrueAvailable( x, y, level, incr ) {
+
+		const { locks } = this;
+		const key = getKey( x, y, level );
+		this._initLock( x, y, level );
+		locks[ key ].present += incr ? 1 : - 1;
+
+		this._resolveEvents( key );
 
 	}
 
