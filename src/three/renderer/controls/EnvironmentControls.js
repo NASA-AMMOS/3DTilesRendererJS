46b140d274470521b64231e18e48131b6d2259b6
heycharlieabbott <chzabbott@gmail.com>
2026-08-07 19:42:28 +0900

    Fix Orthographic camera zoomSpeed calculation
    Apply orthographic zoomSpeed in normalizedDelta calculation to prevent
    flipped/runaway zoom values

--- src/three/renderer/controls/EnvironmentControls.js
+++ src/three/renderer/controls/EnvironmentControls.js
@@ -1283,10 +1283,8 @@ export class EnvironmentControls extends EventDispatcher {
 			_mouseBefore.unproject( camera );
 
 			// zoom the camera
-			const normalizedDelta = Math.pow( 0.95, Math.abs( scale * 0.05 ) );
+			const normalizedDelta = Math.pow( 0.95, Math.abs( scale * 0.05 ) * zoomSpeed );
 			let scaleFactor = scale > 0 ? 1 / Math.abs( normalizedDelta ) : normalizedDelta;
-			scaleFactor *= zoomSpeed;
-
 			if ( scaleFactor > 1 ) {
 
 				if ( maxZoom < camera.zoom * scaleFactor ) {
