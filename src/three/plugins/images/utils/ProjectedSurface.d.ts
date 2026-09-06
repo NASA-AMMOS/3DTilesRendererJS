159865d6a845a4ce33337331597ca28dc14d2976
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-09-06 17:31:21 +0900

    Add projected surface d ts

---
+++ src/three/plugins/images/utils/ProjectedSurface.d.ts
@@ -0,0 +1,17 @@
+import { Vector2, Vector3 } from 'three';
+
+export class ProjectedSurface {
+
+	readonly isProjectedSurface: true;
+	projection: object;
+	scale: Vector2;
+	offset: Vector2;
+
+	constructor( projection?: object );
+
+	getCartographicToPosition( lat: number, lon: number, height: number, target: Vector3 ): Vector3;
+	getPositionToCartographic( pos: Vector3, target: object ): { lat: number, lon: number, height: number };
+	getCartographicToNormal( lat: number, lon: number, target: Vector3 ): Vector3;
+	getPositionToNormal( pos: Vector3, target: Vector3 ): Vector3;
+
+}
